79f9ccb4eb5324a71d4d9026bb60f72eccc82209
Eslam Mohammed Saeed <eslam.muhammed.saeed@gmail.com>
2021-11-13 00:51:59 +0200

    Initialize project using Create React App

---
+++ src/data/sources/source.js
@@ -0,0 +1,11 @@
+import { MAP_TYPES } from '@deck.gl/carto';
+
+const SOURCE_ID = 'source';
+
+const source = {
+  id: SOURCE_ID,
+  data: `TYPE YOUR QUERY HERE`,
+  type: MAP_TYPES.QUERY,
+};
+
+export default source;
